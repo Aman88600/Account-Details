3af4816a1c33c30560a9336b6a45db589e30dd21
Aman88600 <109413130+Aman88600@users.noreply.github.com>
2024-02-18 20:16:49 +0530

    Complete App

--- To_Do_list_App.py
+++ To_Do_list_App.py
@@ -3,12 +3,18 @@ tasks = []
 exit = 0
 
 while (exit != 1):
-    choice = int(input(f"Enter 1 to add tasks\n2 to Delete tasks\n3 to view tasks : "))
+    choice = int(input(f"1 to Add Task\n2 to Remove Task\n3 to View Tasks\n4 Exit\nEnter your choice :"))
     if choice == 1:
         task = input(f"Enter your task : ")
         tasks.append(task)
+        print("\n")
     elif choice == 2:
-        for i in tasks:
-            print(f"{i}")
-    elif choice == 0:
+        task_number = int(input("Enter the Task number to remove : "))
+        tasks.pop(task_number - 1)
+        print("\n")
+    elif choice == 3:
+        for index, element in enumerate(tasks, start=1):
+            print(f"{index}. {element}")
+        print("\n")
+    elif choice == 4:
         exit = 1
