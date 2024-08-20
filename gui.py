406a27f7b68f88befde39720fc0be969b4d3b583
Aman88600 <amanbasoya02@gmail.com>
2024-08-20 04:14:49 -0700

    Now we can add our name in the json file using the gui, soon we will be able to add much more

--- gui.py
+++ gui.py
@@ -1,17 +1,26 @@
 import customtkinter
+import json
 
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-# app.geometry("400x240")
+app = customtkinter.CTk()
+app.minsize(400, 200)
 
-def button_function():
-    print("button pressed")
-    button1 = customtkinter.CTkButton(master=app, text="new Button")
-    button1.pack()
-# Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.pack()
+# Setting Title of the screen
+app.title("Account Details")
 
+def set_name():
+    output = name.get()
+    details = {}
+    if output:
+        details["name"] = str(output)
+        print(details)
+        with open("data.json", "w") as fp:
+            json.dump(details, fp, indent=4)
+
+name = customtkinter.CTkEntry(app, placeholder_text="Enter Your Name", width = 200)
+name.grid(row = 0, column = 0, padx = 100, pady = 20)
+name_button = customtkinter.CTkButton(app, text = "Enter", width = 200, command = set_name)
+name_button.grid(row=1, column=0, padx = 100)
 app.mainloop()
