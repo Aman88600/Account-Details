33ef5ff449a2487e351b57dc037470f9bd308e0d
Aman88600 <amanbasoya02@gmail.com>
2024-08-20 02:57:31 -0700

    Protitype of the app multiple detail feature for an entry in command line

--- writing_to_json.py
+++ writing_to_json.py
@@ -1,8 +1,18 @@
 import json
-# file = open("data.json")
-data = {"Name" : "Aman Basoya", "Age" : 21}
-# file.write(x)
-# file.close()
 
+
+number_of_details = int(input("How many details you want to enter"))
+details = {}
+i = 0
+while i < number_of_details:
+    detail_name = str(input("Enter Detail Name : "))
+    detail_data = input("Enter Detail Data : ")
+    details[detail_name] = detail_data
+    i += 1
+print(details)
+# name = str(input("Enter your Name :"))
+# age = int(input("Enter your Age : "))
+
+# data = {"Name" : name, "Age" : age}
 with open("data.json", "w") as fp:
-    json.dump(data, fp, indent=6)
+    json.dump(details, fp, indent=4)
