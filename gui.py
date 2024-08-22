e75eaafcf2d50093374e7953345b921f11195c32
Aman88600 <amanbasoya02@gmail.com>
2024-08-22 02:21:19 -0700

    Still Working on the json database and the gui

--- gui.py
+++ gui.py
@@ -10,24 +10,26 @@ app.minsize(400, 200)
 # Setting Title of the screen
 app.title("Account Details")
 
+# setting the counter
+count = 0
+
 def set_name():
+    global count # Declare count as global to modify it
     output = name.get()
-    details = {}
     if output:
-        details["Entry"] = str(output)
-        print(details)
-        count = 2
         new_entry = customtkinter.CTkButton(app, text = output, width = 200)
-        # new_entry.grid(row = count, column = 0, padx = 100, pady = 20)
         new_entry.pack()
         count += 1
-        with open("data.json", "w") as fp:
-            json.dump(details, fp, indent=4)
+        with open("data.json", "r") as file:
+            data = json.load(file)
+        data[f"{count}"] = output
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent = 4)
 
 name = customtkinter.CTkEntry(app, placeholder_text="Entry Name", width = 200)
-# name.grid(row = 0, column = 0, padx = 100, pady = 20)
 name.pack()
+
 name_button = customtkinter.CTkButton(app, text = "Create New Entry", width = 200, command = set_name)
-# name_button.grid(row=1, column=0, padx = 100)
 name_button.pack()
+
 app.mainloop()
